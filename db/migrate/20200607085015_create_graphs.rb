eebddb10349cb0c6e181f277466adbc349a0d283
silverskg <chokichoki_gm@i.softbank.jp>
2020-06-07 18:00:12 +0900

    各ユーザーが同じ日付のデータを記録できないようバリデーションを付ける

--- db/migrate/20200607085015_create_graphs.rb
+++ db/migrate/20200607085015_create_graphs.rb
@@ -1,11 +1,12 @@
 class CreateGraphs < ActiveRecord::Migration[5.2]
   def change
     create_table :graphs do |t|
-      t.date :date
-      t.float :weight
-      t.references :user, foreign_key: true
+      t.date :date, null: false
+      t.float :weight, null: false
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
+    add_index :graphs, [:user_id, :date], unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_082947) do
+ActiveRecord::Schema.define(version: 2020_06_07_085015) do
+
+  create_table "graphs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.date "date", null: false
+    t.float "weight", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "date"], name: "index_graphs_on_user_id_and_date", unique: true
+    t.index ["user_id"], name: "index_graphs_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +34,5 @@ ActiveRecord::Schema.define(version: 2020_06_07_082947) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "graphs", "users"
 end

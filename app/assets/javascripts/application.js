b025e905ec236762001747cc93a6215c512e6149
silverskg <chokichoki_gm@i.softbank.jp>
2020-06-07 17:06:30 +0900

    appjsに各種追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,11 @@
 //
 //= require rails-ujs
 //= require activestorage
+//= require Chart.min
+//= require flatpickr
+//= require flatpickr/l10n/ja
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .

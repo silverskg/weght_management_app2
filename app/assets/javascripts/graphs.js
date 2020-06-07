81bb71e4ccb1f71fbbde005e1963e345779ae031
silverskg <chokichoki_gm@i.softbank.jp>
2020-06-07 17:47:02 +0900

    scriptタグからjsに直接記入

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -1,2 +1,39 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+document.addEventListener('turbolinks:load', () => {
+  let barLabel = ['1月', '2月', '3月', '4月', '5月', '6月']
+  let barData = [5, 4, 2, 6, 5, 8]
+  
+  const barChartData = {
+        labels: barLabel,
+        datasets: [{
+            label: '得点',
+            data: barData,
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1
+        }]
+    }
+
+    const barChartOption = {
+        title: {
+            display: true,
+            text: '棒グラフ'
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    // y軸のメモリを 0 からスタートに強制
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+
+    const barChartContext = document.getElementById("bar-chart").getContext('2d')
+    new Chart(barChartContext, {
+        type: 'bar',
+        data: barChartData,
+        options: barChartOption
+    })
+
+
+})

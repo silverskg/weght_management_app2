4d2a3b8e7bc781037c4c9b028fcabfcb9c739aa2
silverskg <chokichoki_gm@i.softbank.jp>
2020-06-07 17:32:30 +0900

    ログイン関連ページを日本語化

--- config/application.rb
+++ config/application.rb
@@ -19,15 +19,9 @@ Bundler.require(*Rails.groups)
 
 module WeightManagementApp2
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo'
     config.generators.system_tests = nil
   end
 end

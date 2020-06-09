5c600c8a564ac729ae73d9f7362be1e8766f5755
silverskg <chokichoki_gm@i.softbank.jp>
2020-06-09 22:42:36 +0900

    新規登録モーダル作成

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -2,4 +2,11 @@ class GraphsController < ApplicationController
   def index
     gon.weight_records = Graph.chart_data(current_user)
   end
+
+  def create
+  end
+
+  def update
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'graphs#index'
+
+  resource :graphs, only: %i[index create update]
 end

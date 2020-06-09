baedf6b61edafa5d7b22fc8b90ef116e0bd34532
silverskg <chokichoki_gm@i.softbank.jp>
2020-06-09 23:05:49 +0900

    データ新規登録

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -5,9 +5,26 @@ class GraphsController < ApplicationController
   end
 
   def create
+    # binding.pry
+    @graph = current_user.graphs.build(graph_params)
+    date = @graph.date.strftime('%Y/%-m/%-d')
+
+    if @graph.save
+      flash[:notice] = "#{date}の記録を追加しました"
+    else
+      flash[:alert] = 'エラーが発生しました'
+    end
+
+    redirect_to root_path
   end
 
   def update
   end
 
+
+  private
+
+  def graph_params
+    params.require(:graph).permit(:date, :weight)
+  end
 end

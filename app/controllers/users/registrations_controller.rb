068bce77b334fc7432b8573b8806b9d40f283136
silverskg <chokichoki_gm@i.softbank.jp>
2021-05-06 14:12:29 +0900

    パスワード再設定無効

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :ensure_normal_user, only: :destroy
+  before_action :ensure_normal_user, only: %i[destroy update]
 
   def ensure_normal_user
     if resource.email == "guest@example.com"
-      redirect_to root_path, alert: "ゲストユーザーは削除できません"
+      redirect_to root_path, alert: "ゲストユーザーは更新・削除できません"
     end
   end
 end

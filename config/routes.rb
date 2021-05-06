d750dc349155696db136fff1f43198601620596a
silverskg <chokichoki_gm@i.softbank.jp>
2021-05-06 12:46:39 +0900

    ゲストログイン機能-ルーティグ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'graphs#index'
-
   resource :graphs, only: %i[index create update]
+
+  devies_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
 end

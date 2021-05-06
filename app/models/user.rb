194cf3ffa9d9894b289d212c473e1b7cd73412f5
silverskg <chokichoki_gm@i.softbank.jp>
2021-05-06 12:54:15 +0900

    email取得確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :graphs, dependent: :destroy
+
+  def self.guest
+    find_or_create_by!(email: "guest@example.com") do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
 end
